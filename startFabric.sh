7e0786282205a6b19c7f6842c49e244bfb53295e
saif-lesnar <saif_lesnar@outlook.com>
2021-08-09 23:32:43 +0600

    Full flow migrated to docker stack

--- startFabric.sh
+++ startFabric.sh
@@ -26,10 +26,12 @@ rm -rf express-app/myapp/wallet/*
 
 # launch network; create channel and join peer to channel
 # pushd ../test-network
-# ./network.sh down
-# ./network.sh up -ca -s couchdb
+./network.sh down
+./network.sh up -ca -s couchdb
 # ./network.sh up createChannel -ca -s couchdb
-# ./network.sh createChannel
+sleep 10
+./network.sh createChannel
+sleep 10
 ./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl ${CC_SRC_LANGUAGE} -ccp ${CC_SRC_PATH}
 # popd test-network
 
