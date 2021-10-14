fd8b73bf2aaafc0c393b330e88efb0f500e724b4
saif-lesnar <saif_lesnar@outlook.com>
2021-10-14 14:41:36 +0600

    jq testing

--- startFabric.sh
+++ startFabric.sh
@@ -40,8 +40,14 @@ sleep 10
 adminPrivateKeyOrg1MSP=$(ls organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/)
 adminPrivateKeyOrg2MSP=$(ls organizations/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/)
 
-adminPrivateKeyOrg1MSPPath=$"/tmp/crypto/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp/keystore/$adminPrivateKeyOrg1MSP"
+adminPrivateKeyOrg1MSPPath='asdasd'
 adminPrivateKeyOrg2MSPPath=$"/tmp/crypto/peerOrganizations/org2.example.com/users/User1@org2.example.com/msp/keystore/$adminPrivateKeyOrg2MSP"
+
+
+mv connection-profile/test-network.json connection-profile/temp.json
+jq -r ".organizations.Org1MSP.adminPrivateKey.path |= '$adminPrivateKeyOrg1MSPPath'" connection-profile/temp.json > connection-profile/test-network.json
+rm connection-profile/temp.json
+
 
 jq .organizations.Org1MSP.adminPrivateKey.path = adminPrivateKeyOrg1MSPPath connection-profile/test-network.json 
 jq .organizations.Org2MSP.adminPrivateKey.path = adminPrivateKeyOrg2MSPPath connection-profile/test-network.json 
